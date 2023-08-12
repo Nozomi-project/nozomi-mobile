7e8ac3e79870fae9f9d18b76fcd86f73b1557945
Sofia Toro <sofie.torch@outlook.com>
2023-08-12 17:52:42 -0400

    update styles to material 3

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,22 @@
+part of 'app_theme.dart';
+
+final _textTheme = TextTheme(
+  displayLarge: _poppinsTextTheme.displayLarge,
+  displayMedium: _poppinsTextTheme.displayMedium,
+  displaySmall: _poppinsTextTheme.displaySmall,
+  headlineLarge: _poppinsTextTheme.headlineLarge,
+  headlineMedium: _poppinsTextTheme.headlineMedium,
+  headlineSmall: _poppinsTextTheme.headlineSmall,
+  titleLarge: _poppinsTextTheme.titleLarge,
+  titleMedium: _poppinsTextTheme.titleMedium,
+  titleSmall: _poppinsTextTheme.titleSmall,
+  bodyLarge: _latoTextTheme.bodyLarge,
+  bodyMedium: _latoTextTheme.bodyMedium,
+  bodySmall: _latoTextTheme.bodySmall,
+  labelLarge: _latoTextTheme.labelLarge,
+  labelMedium: _latoTextTheme.labelMedium,
+  labelSmall: _latoTextTheme.labelSmall,
+);
+
+final _poppinsTextTheme = GoogleFonts.poppinsTextTheme();
+final _latoTextTheme = GoogleFonts.latoTextTheme();
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,3 +1,2 @@
 export 'app_colors.dart';
-export 'app_text_style.dart';
 export 'app_theme.dart';
